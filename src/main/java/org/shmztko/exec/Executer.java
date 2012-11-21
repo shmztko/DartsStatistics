7fa186db07690eb70c09aa989c6bcdfbc51217c0
takewo <st0098@gmail.com>
2012-11-21 10:46:44 +0900

    データベースへのアクセスはDataBaseManagerを使うように変更

--- src/main/java/org/shmztko/exec/Executer.java
+++ src/main/java/org/shmztko/exec/Executer.java
@@ -1,8 +1,5 @@
 package org.shmztko.exec;
 
-import java.sql.SQLException;
-
-import org.shmztko.exceptions.ApplicationException;
 import org.shmztko.model.DataBaseManager;
 import org.shmztko.model.User;
 import org.shmztko.recorder.DartsStatsRecorder;
@@ -10,16 +7,11 @@ import org.shmztko.recorder.DartsStatsRecorder;
 public class Executer {
 
 	public static void main(String[] args) {
-		try {
-			User[] users = DataBaseManager.getEntityManager().find(User.class);
-			DartsStatsRecorder recorder = new DartsStatsRecorder();
+		User[] users = DataBaseManager.getInstance().find(User.class);
+		DartsStatsRecorder recorder = new DartsStatsRecorder();
 
-			for (User user : users) {
-				recorder.record(user);
-			}
-
-		} catch (SQLException e) {
-			throw new ApplicationException(e);
+		for (User user : users) {
+			recorder.record(user);
 		}
 	}
 }
