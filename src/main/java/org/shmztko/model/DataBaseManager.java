92af52de59f18af575b045305450f6ac977182f6
takewo <st0098@gmail.com>
2012-11-21 10:12:54 +0900

    EntityManager操作用クラスの作成

--- src/main/java/org/shmztko/model/DataBaseManager.java
+++ src/main/java/org/shmztko/model/DataBaseManager.java
@@ -1,9 +1,15 @@
 package org.shmztko.model;
 
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
+import net.java.ao.DBParam;
 import net.java.ao.DatabaseProvider;
+import net.java.ao.Entity;
 import net.java.ao.EntityManager;
+import net.java.ao.Query;
+
+import org.shmztko.exceptions.ApplicationException;
 
 public class DataBaseManager {
 
@@ -22,5 +28,45 @@ public class DataBaseManager {
 
 	public static EntityManager getEntityManager() {
 		return INSTANCE.manager;
+	}
+
+	public static DataBaseManager getInstance() {
+		return INSTANCE;
+	}
+
+	public <T extends Entity> T create(Class<T> entity, DBParam ...dbParams) {
+		try {
+			return manager.create(entity, dbParams);
+		} catch (SQLException e) {
+			throw new ApplicationException("Failed to create records. -> " + entity.toString(), e);
+		}
+	}
+
+	public void migrate(Class<? extends Entity> ... entities) {
+		try {
+			manager.migrate(entities);
+		} catch (SQLException e) {
+			throw new ApplicationException("Failed to migrate. -> " + entities.toString(), e);
+		}
+	}
+
+	public <T extends Entity> T[] find(Class<T> entity, Query query) {
+		try {
+			return manager.find(entity, query);
+		} catch (SQLException e) {
+			throw new ApplicationException("Failed to find records. -> " + entity.toString(), e);
+		}
+	}
+
+	public <T extends Entity> T[] find(Class<T> entity) {
+		try {
+			return manager.find(entity);
+		} catch (SQLException e) {
+			throw new ApplicationException("Failed to find records. -> " + entity.toString(), e);
+		}
+	}
+
+	public <T extends Entity> T[] get(Class<T> entity) {
+		return manager.get(entity);
 	}
 }
