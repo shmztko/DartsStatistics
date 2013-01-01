042c481f9a7305ce394ab51c1b690715adc7d8f9
takewo <st0098@gmail.com>
2013-01-01 15:38:11 +0900

    実行用シェル作成

---
+++ src/main/assembly/dartsstat.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# /dartsstat.sh AddUser st0098@gmail.com たけを＠紫推し http://card.dartslive.com/t/top.jsp?i=559300205543375&n=2124119876
+java -cp dartsstats-0.0.1-SNAPSHOT.jar:./dependency/* org.shmztko.exec.$1 $2 $3 $4 $5
