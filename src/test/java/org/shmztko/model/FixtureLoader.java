83313b48ab698d2c9adc98e0bd8330ada99ad848
takewo <st0098@gmail.com>
2013-01-02 16:57:28 +0900

    コメント記述

--- src/test/java/org/shmztko/model/FixtureLoader.java
+++ src/test/java/org/shmztko/model/FixtureLoader.java
@@ -13,10 +13,22 @@ import net.java.ao.schema.CamelCaseFieldNameConverter;
 import org.ho.yaml.Yaml;
 import org.ho.yaml.exception.YamlException;
 
+/**
+ * RailsのFIXTURE風に、YAMLを読み込みオブジェクトを生成するクラス。
+ * @author ShimizuTakeo
+ */
 public class FixtureLoader {
 
+	/** FIXTUREの読み込み先パス */
 	private static final String FIXTURE_PATH = "src/test/resources/fixtures";
 
+	/**
+	 * FIXTUREファイルをロードします。
+	 * @param <T> ロード対象クラスの型
+	 * @param entity ロード対象のクラス
+	 * @return ロード結果
+	 */
+	@SuppressWarnings("unchecked")
 	public <T extends Entity> Map<String, T> load(Class<T> entity) {
 		try {
 			Map<Object, Object> ymlMap = (Map<Object, Object>)Yaml.load(getYaml(entity));
@@ -49,6 +61,11 @@ public class FixtureLoader {
 		}
 	}
 
+	/**
+	 * ロード対象クラスの名前からYAMLファイルを取得します。
+	 * @param entity ロード対象クラス
+	 * @return YAMLファイルの参照
+	 */
 	private File getYaml(Class<? extends Entity> entity) {
 		return new File(FIXTURE_PATH + "/" + entity.getSimpleName() + ".yml");
 	}
