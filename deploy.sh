ccb233bb67496bc45fe24a2f89df46c8eb5dec21
takewo <st0098@gmail.com>
2013-01-07 14:27:59 +0900

    デプロイ用のシェルでMavenのビルドも行うように変更

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,31 @@
 #!/bin/sh
 echo 'deploy start'
 
+deploy_root=./target/dartsstat
+
+echo 'maven build start'
+phase=$1
+# Proxy環境でテストするときは
+# mvn clean install -P test -Dhttp.proxyHost=xxx.xxx.xxx.xxx -Dhttp.proxyPort=nnnn
+if [ "$phase" = "" ]
+then
+	mvn clean install -P test
+else
+	mvn clean install -P $phase
+fi
+
 echo 'create deploy dir'
-mkdir -p dartsstat/lib
+mkdir -p $deploy_root/lib
 
 echo 'copy dependencies'
-cp ./target/dependency/* dartsstat/lib
-cp ./target/dartsstats-*.jar dartsstat
-cp ./src/main/assembly/dartsstat.sh dartsstat
+cp ./target/dependency/* $deploy_root/lib
+cp ./target/dartsstats-*.jar $deploy_root
+cp ./src/main/bin/dartsstat.sh $deploy_root
 
 echo 'archive package'
-tar cf - dartsstat | gzip > dartsstat.tar.gz
+tar cf - $deploy_root | gzip > $deploy_root.tar.gz
 
 echo 'delete unused dir'
-rm -rf ./dartsstat
+rm -rf $deploy_root
 
 echo 'deply end'
