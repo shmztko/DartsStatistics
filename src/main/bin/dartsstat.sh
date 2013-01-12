556ec0c2341acccbebbc6e5d6d507762c60cb496
takewo <st0098@gmail.com>
2013-01-12 10:43:59 +0900

    実行用シェルファイルが特定のディレクトリでしか動いていないのを修正

--- src/main/bin/dartsstat.sh
+++ src/main/bin/dartsstat.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # /dartsstat.sh AddUser st0098@gmail.com たけを＠紫推し http://card.dartslive.com/t/top.jsp?i=559300205543375&n=2124119876
-java -cp dartsstats-0.0.1-SNAPSHOT.jar:./dependency/* org.shmztko.exec.$1 $2 $3 $4 $5
+BASE_DIR=/home/takewo/dartsstat
+java -cp $BASE_DIR/dartsstats-0.0.1-SNAPSHOT.jar:$BASE_DIR/lib/* -Dfile.encoding=utf-8 org.shmztko.exec.$1 $2 $3 $4 $5
